43b6674313456e7dd139ac59e39c67968c41e08a
Masullo <masullowebdev@gmail.com>
2025-01-31 17:13:45 -0400

    forcing session route to be dynamic

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import { options } from "../auth/[...nextauth]/options";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(){
     try{
         const session = await getServerSession(options)
